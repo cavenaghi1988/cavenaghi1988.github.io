9ed4ed6f48210db5406a5e02532587fecb89d0ea
john <cavenaghi1988@yahoo.gr>
2019-12-19 19:41:44 +0300

    first final

---
+++ src/js/script.js
@@ -0,0 +1,57 @@
+$(document).ready(function(){
+    let slider = tns({
+        container: '.carousel__inner',
+        items: 1,
+        slideBy: 'page',
+        autoplay: false,
+        controls: false,
+        nav: false,
+        responsive: {
+            640: {
+              
+            },
+            700: {
+              
+            },
+            900: {
+              
+            }
+          }
+      });
+    
+      let sliderPrev = document.querySelector('.prev'),
+          sliderNext = document.querySelector('.next');
+    
+      
+      sliderPrev.addEventListener('click', function(){
+          slider.goTo('prev');
+      });
+    
+      sliderNext.addEventListener('click', function() {
+          slider.goTo('next');
+      });
+    
+      $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+      }); 
+
+
+        function toggleSlide(item) {
+            $(item).each(function(i){
+                $(this).on('click', function(e){
+                    e.preventDefault();
+                    $('.catalog-item__cont').eq(i).toggleClass('catalog-item__cont_active');
+                    $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+                });
+            });
+        }
+
+        toggleSlide('.catalog-item__back');
+        toggleSlide('.catalog-item__link');
+
+
+    
+
+});
